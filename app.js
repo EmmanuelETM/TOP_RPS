a8d23143f908e63aa89902c25be5dae535beb1db
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-09-24 22:10:48 -0400

    created getHumanChoice(), getComputerChoice(), and working on playRound()

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('hello world');
+const getComputerChoice = () => {
+    let numChoice = Math.floor((Math.random() * 3) + 1);
+    if (numChoice === 1) return 'rock'
+    else if (numChoice === 2) return 'paper'
+    else return 'scissors';
+}
+
+const getHumanChoice = () => {
+    const choice = prompt(`
+        Welcome to rock, paper, scissors.
+        Select your move:
+        `);
+    return choice;
+}
+
+let humanScore, computerScore = 0;
+let rules = {
+    rock: 'paper',
+    paper: 'scissors',
+    scissors: 'rock',
+}
+
+console.log(rules)
+const playRound = (rules, humanChoice, computerChoice) => {
+    humanChoice.toLowerCase()
+    if(humanChoice != 'rock' || humanChoice != 'paper' || humanChoice != 'scissors') return `Error:  ${humanChoice} is not an option try again`
+
+
+} 
+
+
+// console.log(playRound(rules, getHumanChoice(), getComputerChoice()))
